6a2d49a17c7793846601363a25235e1aaefd60c3
wahyua111 <85115384+wahyua111@users.noreply.github.com>
2022-08-20 03:57:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((1808136434 5155299861 1297715519 1633841964))
+ADMINS.extend((1808136434, 5155299861, 1297715519, 1633841964))
 
 
 LOG_FILE_NAME = "logs.txt"
